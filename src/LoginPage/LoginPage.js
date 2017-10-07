c52751e58d20139143457a27ab66fbb7be873c3b
ArkejGit <robert.krawczyk92@gmail.com>
2017-10-07 18:41:51 +0200

    Improve login page view

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './LoginPage.css';
 
 const LoginPage = (props) => {
 	return (
-		<div className="container text-center">
+		<div className="container text-center col-sm-6 col-sm-offset-3">
+				<h1>Hahathon TicTacToe Game</h1>
 				<p>name</p>
 				<input onChange={props.onLoginChange}
 				type="text" className="form-control" aria-label="Text input with checkbox"/>
+				<br/>
 				<p>password</p>
 				<input onChange={props.onPasswordChange}
 				type="password" className="form-control" aria-label="Text input with checkbox"/>
 				<br/>
-				<div>
+				<div className="buttons">
 					<button onClick={props.clickRegister} 
 					type="button" className="btn btn-secondary">Register
 					</button>
